36e62896187875eff52c0028e01a4664c85ccea3
raphafernandes83 <raphainformaticacampogrande@gmail.com>
2026-08-02 16:06:05 -0300

    Atualiza endpoint do formulário para nova implantação Apps Script

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.GP_FORM_CONFIG={endpoint:"https://script.google.com/macros/s/AKfycbzIhiYoxnR1siPFb7NJDVRR2Lnadlr5Ay05_5IseMIvf9nAt4BNoCd3PBP0oYfsWqInxw/exec",requestTimeoutMs:15000};
+window.GP_FORM_CONFIG={endpoint:"https://script.google.com/macros/s/AKfycbzTCSYq3PcDAjmIJ3LuqyPgBgjGZ-PLOW63lBH2_RFCgQZp9W0hOiDIQMgndNzmjfsuUg/exec",requestTimeoutMs:15000};
