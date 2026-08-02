9c2531e7a3a75b02dafea6b2f3ab8f97dd093c87
raphafernandes83 <raphainformaticacampogrande@gmail.com>
2026-08-02 15:32:06 -0300

    Conecta formulário ao Google Apps Script

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.GP_FORM_CONFIG={endpoint:"COLE_AQUI_A_URL_DO_GOOGLE_APPS_SCRIPT",requestTimeoutMs:15000};
+window.GP_FORM_CONFIG={endpoint:"https://script.google.com/macros/s/AKfycbzIhiYoxnR1siPFb7NJDVRR2Lnadlr5Ay05_5IseMIvf9nAt4BNoCd3PBP0oYfsWqInxw/exec",requestTimeoutMs:15000};
