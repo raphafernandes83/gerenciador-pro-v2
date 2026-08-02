8db0cbe47fa704ea1f97bc59cf34843e6337fde9
raphafernandes83 <raphainformaticacampogrande@gmail.com>
2026-08-02 16:21:36 -0300

    Conecta formulário à nova implantação do Apps Script

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.GP_FORM_CONFIG={endpoint:"https://script.google.com/macros/s/AKfycbzTCSYq3PcDAjmIJ3LuqyPgBgjGZ-PLOW63lBH2_RFCgQZp9W0hOiDIQMgndNzmjfsuUg/exec",requestTimeoutMs:15000};
+window.GP_FORM_CONFIG={endpoint:"https://script.google.com/macros/s/AKfycbyVp3xSRPt0cTEQsPxuFs9vNJzZBMuI4ctPqJPWqChxlWCY_hYgVLboRbnpzBkW2vAX/exec",requestTimeoutMs:15000};
